733fd6ccfb82a33c38a05e2ff7fbc8cbc82bbad3
royyan <yanlongjie@msn.com>
2026-01-18 15:12:14 +1000

    Update __init__.py

--- components/freestyle_lock/__init__.py
+++ components/freestyle_lock/__init__.py
@@ -1,5 +1,4 @@
-# components/freestyle_lock/__init__.py
-
+# Minimal working registration - no extras, just proves loading
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import lock
@@ -7,22 +6,15 @@ from esphome.const import CONF_ID, CONF_NAME
 
 DEPENDENCIES = ["esp32_ble_client"]
 
-freestyle_lock_ns = cg.esphome_ns.namespace("freestyle_lock")
-FreestyleLock = freestyle_lock_ns.class_("FreestyleLock", lock.Lock, cg.Component)
-
-CONF_BLE_MAC = "ble_mac"
-CONF_AES_KEY = "aes_key"
+ns = cg.esphome_ns.namespace("freestyle_lock")
+FreestyleLock = ns.class_("FreestyleLock", lock.Lock, cg.Component)
 
 CONFIG_SCHEMA = lock.LOCK_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(FreestyleLock),
     cv.Required(CONF_NAME): cv.string,
-    cv.Required(CONF_BLE_MAC): cv.string_strict,
-    cv.Required(CONF_AES_KEY): cv.string_strict,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await lock.register_lock(var, config)
-    cg.add(var.set_ble_mac(config[CONF_BLE_MAC]))
-    cg.add(var.set_aes_key(config[CONF_AES_KEY]))
